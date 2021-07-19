7833158cf234758d40a0a842e123e8928cf2862c
juanDeGiorgi <juanmadegiorgi@gmail.com>
2021-07-19 03:32:22 -0300

    add procfile

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ const logger = require('morgan');
 const express = require("express");
 const app = express();
 const path = require("path");
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 
 // CONFIG
